e77e3d08fa61a235230ea2259f30811e653b78ca
nilshoerrmann <nils.hoerrmann@hananils.de>
2020-09-11 09:35:43 +0200

    Check if method exists first

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-function snippetWhen($snippet, $condition, $expected = true, $data = [])
-{
-    if (is_string($condition)) {
-        $condition = page()->is($condition);
-    }
+if (!function_exists('snippetWhen')) {
+    function snippetWhen($snippet, $condition, $expected = true, $data = [])
+    {
+        if (is_string($condition)) {
+            $condition = page()->is($condition);
+        }
 
-    if ($condition === $expected) {
-        echo snippet($snippet, $data);
+        if ($condition === $expected) {
+            echo snippet($snippet, $data);
+        }
     }
 }
